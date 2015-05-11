2e8def5b351861bb24d69730b65de3aba45eca57
= <=>
2015-05-11 20:10:40 +0000

    Added Item service

---
+++ services/items.js
@@ -0,0 +1,21 @@
+var Item = require('../models/item');
+
+exports.save = function(name, callback, errback) {
+  Item.create({ name: name}, function(err, item) {
+    if (err) {
+      errback(err);
+      return;
+    }
+    callback(item);
+  });
+};
+
+exports.list = function(callback, errback) {
+  Item.find(function(err, items) {
+    if (err) {
+      errback(err);
+      return;
+    }
+    callback(items);
+  });
+};
