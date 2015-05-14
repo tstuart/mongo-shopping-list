a8ae2961b25ad5416f00069bd7657c7d71e762c3
= <=>
2015-05-14 01:54:38 +0000

    Working on adding delete and edit functionality

--- routes/item.js
+++ routes/item.js
@@ -13,10 +13,22 @@ router.get('/items', function(req, res) {
 
 router.post('/items', function(req, res) {
   Item.save(req.body.name, function(item){
+    res.status(201).json(item);    
+  }, function(err) {
+    res.status(400).json(err);
+  });
+});
+
+router.delete('/items/:id', function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  /*
+  Item.delete(id, function(item){
     res.status(201).json(item);
   }, function(err) {
     res.status(400).json(err);
   });
+  */
 });
 
 module.exports = router;
--- services/items.js
+++ services/items.js
@@ -1,27 +1,13 @@
 var Item = require('../models/item');
 
 exports.save = function(name, callback, errback) {
-  
-  // The findOneAndUpdate method will add a new 
-  // item if one does not exist (set upsert to true).
-  // we can use this for adding or updating
-  Item.findOneAndUpdate({ name: name}, { name: name}, {upsert: true}, function(err, item) {
-    if (err) {
-      errback(err);
-      return;
-    }
-    callback(item);
-  });
-  /*
   Item.create({ name: name}, function(err, item) {
     if (err) {
       errback(err);
       return;
     }
     callback(item);
-  });
-  */
-  
+  });  
 };
 
 exports.list = function(callback, errback) {
@@ -31,5 +17,15 @@ exports.list = function(callback, errback) {
       return;
     }
     callback(items);
+  });
+};
+
+exports.delete = function(id, callback, errback) {
+  Item.findByIdAndRemove(id, function(item){
+    if (!item) {
+      errback({"error": "Invalid ID supplied.", "id": id});
+      return;
+    } 
+     callback(item);
   });
 };
